c776b5551dd81de50e629cdde656adca748b014a
Behdad BahramAbadian <Behdad.abadian@gmail.com>
2025-01-31 23:40:24 +0330

    add class libraries

---
+++ Code/ZipLink/ZipLink.Presentation/Areas/Administration/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZipLink.Presentation.Areas.Administration.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
